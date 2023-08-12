6da48d1a3a5cb0f450cdfcc7a1101e101b50a0e8
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2023-08-12 16:44:40 +0900

    stash

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,19 +4,11 @@ import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import fetchContractData from "../util/genNFTData";
 
-// Data structure and constants related to the map
 type Stop = {
   position: LatLngTuple;
   name: string;
   description: string;
   imageURL: string;
-};
-
-const OFFSET = 0.0001; // オフセット量を定義
-
-const offsetPosition = (position: LatLngTuple, count: number): LatLngTuple => {
-  // 位置をわずかにずらす
-  return [position[0] + OFFSET * count, position[1] + OFFSET * count];
 };
 
 const ipfsURLtoHTTP = (ipfsURL: string) => {
@@ -31,21 +23,72 @@ const moveIcon = new L.Icon({
   popupAnchor: [1, -34],
 });
 
-const SelectedStopInfo: React.FC<{ stop: Stop; key: any }> = ({ stop }) => (
-  <div style={{ position: "absolute", bottom: 10, right: 10 }}>
-    <h2>Selected Stop: {stop.name}</h2>
-    <img
-      src={stop.imageURL}
-      alt={stop.name}
-      style={{ width: "100%", height: "auto" }}
-    />
-    <p>description: {stop.description}</p>
-  </div>
-);
+const StopsPopup: React.FC<{ stops: Stop[] }> = ({ stops }) => {
+  const [currentIdx, setCurrentIdx] = useState(0);
+
+  const buttonStyle: React.CSSProperties = {
+    background: "#007BFF",
+    border: "none",
+    borderRadius: "5px",
+    color: "#FFF",
+    padding: "5px 15px",
+    margin: "5px",
+    cursor: "pointer",
+    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+    transition: "all 0.2s ease-in-out",
+  };
+
+  const buttonHoverStyle: React.CSSProperties = {
+    background: "#0056b3",
+  };
+
+  const handleMouseOver = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    Object.assign(event.currentTarget.style, buttonHoverStyle);
+  };
+
+  const handleMouseOut = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    Object.assign(event.currentTarget.style, buttonStyle);
+  };
+
+  return (
+    <div>
+      <h2>{stops[currentIdx].name}</h2>
+      <img
+        src={stops[currentIdx].imageURL}
+        alt={stops[currentIdx].name}
+        style={{ width: "100%", height: "auto" }}
+      />
+      <p>Description: {stops[currentIdx].description}</p>
+      <button
+        style={buttonStyle}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={() =>
+          setCurrentIdx((prev) => (prev - 1 + stops.length) % stops.length)
+        }
+        disabled={stops.length <= 1}
+      >
+        戻る
+      </button>
+      <button
+        style={buttonStyle}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={() => setCurrentIdx((prev) => (prev + 1) % stops.length)}
+        disabled={stops.length <= 1}
+      >
+        次へ
+      </button>
+    </div>
+  );
+};
 
 const Map = () => {
   const [stops, setStops] = useState<Stop[]>([]);
-  const [selectedStop, setSelectedStop] = useState<Stop | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -55,7 +98,6 @@ const Map = () => {
         JSON.parse(jsonString)
       );
 
-      // Convert jsonArray to Stop[]
       const convertedStops: Stop[] = jsonArray.map((data: any) => ({
         position: [
           parseFloat(
@@ -74,20 +116,18 @@ const Map = () => {
         imageURL: ipfsURLtoHTTP(data.image),
       }));
 
-      setStops(convertedStops); // Update stops state with the converted data
-      setLoading(false); // End loading
+      setStops(convertedStops);
+      setLoading(false);
     };
 
     fetchNftData();
   }, []);
 
-  // 位置情報の重複をチェックするためのヘルパー関数
-  const getDuplicateCount = (position: LatLngTuple, index: number): number => {
-    return stops
-      .slice(0, index)
-      .filter(
-        (s) => s.position[0] === position[0] && s.position[1] === position[1]
-      ).length;
+  const getStopsAtPosition = (position: LatLngTuple): Stop[] => {
+    return stops.filter(
+      (stop) =>
+        stop.position[0] === position[0] && stop.position[1] === position[1]
+    );
   };
 
   if (loading) {
@@ -104,36 +144,13 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {stops.map((stop, idx) => {
-        const count = getDuplicateCount(stop.position, idx);
-        const offsettedPosition = offsetPosition(stop.position, count);
-
-        return (
-          <Marker
-            position={offsettedPosition} // 位置をずらしたものを使用
-            key={idx}
-            icon={moveIcon}
-            eventHandlers={{
-              click: () => {
-                setSelectedStop(stop);
-              },
-            }}
-          >
-            <Popup>
-              <h2>{`${idx + 1}. ${stop.name}`}</h2>
-              <img
-                src={stop.imageURL}
-                alt={stop.name}
-                style={{ width: "100%", height: "auto" }}
-              />
-              <p>Description: {stop.description}</p>
-            </Popup>
-          </Marker>
-        );
-      })}
-      {selectedStop && (
-        <SelectedStopInfo stop={selectedStop} key={selectedStop.name} />
-      )}
+      {stops.map((stop, idx) => (
+        <Marker position={stop.position} key={idx} icon={moveIcon}>
+          <Popup>
+            <StopsPopup stops={getStopsAtPosition(stop.position)} />
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
